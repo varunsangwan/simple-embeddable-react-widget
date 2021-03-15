4fa1a17dbfc24051a12c53e9e83eb7357e7f3741
Varun Sangwan <vsangwan5.vs@gmail.com>
2021-03-15 10:23:45 +0530

    Adding values

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,27 +1,37 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-import Config from '../../config';
+import {
+    fetchItem,
+    ElasticSearch,
+  } from "../../config";
 import './widget.css';
 
-const widgetName = Config.name;
+const widgetName = 'Mintable_Widget';
 
 class Widget extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             message: null,
+            formData:null,
         };
     }
-
+    
+      
+    componentDidUpdate(){
+        this.fetchItem(this.state.formData);
+    }
+      
     render() {
         if (this.state.message) {
-            return <div className="widget-container"><h1>I'm a {widgetName}</h1><div>I have a message: {this.state.message}</div></div>;
+            return <div className="widget-container"><h1>I'm {widgetName}</h1><div>I have a message: {this.state.message}</div></div>;
         }
         else {
             return <div className="widget-container"><h1>I'm a {widgetName}</h1></div>;
         }
     }
-
+    setFormData(data){
+        this.setState({formData:data});
+    }
     setMessage(message){
         this.setState({message: message});
     }
